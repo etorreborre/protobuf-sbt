ebd060f3510d5c5d6ef5231cbc48d97fdd1d57ff
Gerolf Seitz <gerolf.seitz@gmail.com>
2010-10-12 06:53:25 +0800

    let projects modify the include path via overriding protobufIncludePath

--- src/main/scala/protobuf.scala
+++ src/main/scala/protobuf.scala
@@ -8,13 +8,15 @@ trait ProtobufCompiler extends DefaultProject {
   def protobufDirectory = "src" / "main" / "protobuf"
   def protobufSchemas = protobufDirectory ** "*.proto"
   def protobufOutputPath = "src" / "main" / "java"
+  def protobufIncludePath = List(protobufDirectory)
   
   def generateProtobufAction = task {
     for (schema <- protobufSchemas.get) {
       log.info("Compiling schema %s".format(schema))
     }
     protobufOutputPath.asFile.mkdirs()
-    <x>protoc -I {protobufDirectory.absolutePath} --java_out={protobufOutputPath.absolutePath} {protobufSchemas.getPaths.mkString(" ")}</x> ! log
+    val incPath = protobufIncludePath.map(_.absolutePath).mkString("-I ", " -I ", "")
+    <x>protoc {incPath} --java_out={protobufOutputPath.absolutePath} {protobufSchemas.getPaths.mkString(" ")}</x> ! log
     None
   } describedAs("Generates Java classes from the specified Protobuf schema files.")
   lazy val generateProtobuf = generateProtobufAction
